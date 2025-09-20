0d95e9fb1ba373ac7ca2447e701237821ff3b701
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-21 00:49:53 +0530

    Created Aircraft.java
    Entity for Aircraft

--- src/main/java/com/airline/fleetmanagement/entity/Aircraft.java
+++ src/main/java/com/airline/fleetmanagement/entity/Aircraft.java
@@ -16,4 +16,60 @@ public class Aircraft {
     private String manufacturer;
     private int capacity;
 
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    public Aircraft(){}
+
+    public Aircraft(String registrationNumber,String model,String manufacturer,int capacity, Status status){
+        this.registrationNumber= registrationNumber;
+        this.model=model;
+        this.manufacturer=manufacturer;
+        this.capacity=capacity;
+        this.status=status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegistrationNumber(){
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
