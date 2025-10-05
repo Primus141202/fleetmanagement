566e3593ef0c36b33b81a1b8f95a99d9a777c668
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-10-05 16:44:18 +0530

    Update AircraftRepository.java

--- src/main/java/com/airline/fleetmanagement/repository/AircraftRepository.java
+++ src/main/java/com/airline/fleetmanagement/repository/AircraftRepository.java
@@ -2,7 +2,9 @@ package com.airline.fleetmanagement.repository;
 
 import com.airline.fleetmanagement.entity.Aircraft;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AircraftRepository extends JpaRepository<Aircraft,Long> {
     Aircraft findByRegistrationNumber(String registrationNumber);
 }
