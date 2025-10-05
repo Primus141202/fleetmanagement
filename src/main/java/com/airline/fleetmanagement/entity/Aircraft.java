445075c4303cac5dbce1d68b45d9ededd04b517d
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-10-05 16:44:02 +0530

    Update Aircraft.java
    Changed it for better readability and dtos

--- src/main/java/com/airline/fleetmanagement/entity/Aircraft.java
+++ src/main/java/com/airline/fleetmanagement/entity/Aircraft.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "Aircrafts")
+@Table(name = "aircraft")
 public class Aircraft {
 
     @Id
@@ -28,75 +28,6 @@ public class Aircraft {
     private int yearOfManufacture;
     private int totalFlightHours;
 
-    public Aircraft(){}
 
-    public Aircraft(String registrationNumber,String model,String manufacturer,int capacity, Status status, String lastRouteFlown, int yearOfManufacture, int totalFlightHours){
-        this.registrationNumber= registrationNumber;
-        this.model=model;
-        this.manufacturer=manufacturer;
-        this.capacity=capacity;
-        this.status=status;
-        this.lastRouteFlown=lastRouteFlown;
-        this.yearOfManufacture=yearOfManufacture;
-        this.totalFlightHours=totalFlightHours;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getRegistrationNumber(){
-        return registrationNumber;
-    }
-
-    public void setRegistrationNumber(String registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getManufacturer() {
-        return manufacturer;
-    }
-
-    public void setManufacturer(String manufacturer) {
-        this.manufacturer = manufacturer;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public int getYearofManufacture(){
-        return yearOfManufacture;
-    }
-    public void setYearofManufacture(int yearOfManufacture){
-        this.yearOfManufacture=yearOfManufacture;
-    }
-
-    public int getTotalFlightHours(){
-        return totalFlightHours;
-    }
-
-    public void setTotalFlightHours(int totalFlightHours){
-        this.totalFlightHours=totalFlightHours;
-    }
 }
