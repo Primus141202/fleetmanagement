dfb2abd260977d9f932fafb24612f9df6ead660d
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-10-05 15:49:29 +0530

    Create AircraftRequest.java

---
+++ src/main/java/com/airline/fleetmanagement/dto/AircraftRequest.java
@@ -0,0 +1,13 @@
+package com.airline.fleetmanagement.dto;
+
+public class AircraftRequest {
+
+    private String registrationNumber;
+    private String model;
+    private String manufacturer;
+    private int capacity;
+    private String status;
+    private String lastRouteFlown;
+    private int yearOfManufacture;
+    private int totalFLightHours;
+}
