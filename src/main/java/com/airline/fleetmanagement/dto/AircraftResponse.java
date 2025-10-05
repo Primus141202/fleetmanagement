7e48d766fa18ac363d31dd3f70b6095ecd2af389
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-10-05 16:02:26 +0530

    Update AircraftResponse.java

--- src/main/java/com/airline/fleetmanagement/dto/AircraftResponse.java
+++ src/main/java/com/airline/fleetmanagement/dto/AircraftResponse.java
@@ -4,6 +4,14 @@ import lombok.Data;
 
 @Data
 public class AircraftResponse {
-
+    private Long id;
+    private String registrationNumber;
+    private String model;
+    private String manufacturer;
+    private int capacity;
+    private String status;
+    private String lastRouteFlown;
+    private int yearOfManufacture;
+    private int totalFlightHours;
 
 }
