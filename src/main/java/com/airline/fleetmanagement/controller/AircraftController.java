e2ce541ba8209d3694eb12876bb3508a00ee5863
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-10-05 16:45:02 +0530

    Update AircraftController.java
    Changes made for request and response dtos when apis are called

--- src/main/java/com/airline/fleetmanagement/controller/AircraftController.java
+++ src/main/java/com/airline/fleetmanagement/controller/AircraftController.java
@@ -1,38 +1,44 @@
 package com.airline.fleetmanagement.controller;
 
+import com.airline.fleetmanagement.dto.AircraftRequest;
+import com.airline.fleetmanagement.dto.AircraftResponse;
 import com.airline.fleetmanagement.entity.Aircraft;
+import com.airline.fleetmanagement.mapper.AircraftMapper;
 import com.airline.fleetmanagement.service.AircraftService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.ResponseEntity.*;
 
 @RestController
 @RequestMapping("/api/aircraft")
+@RequiredArgsConstructor
 public class AircraftController {
 
     private final AircraftService aircraftService;
 
-    public AircraftController(AircraftService aircraftService){
-        this.aircraftService=aircraftService;
-    }
-
     @PostMapping
-    public ResponseEntity<Aircraft> addAircraft(@RequestBody Aircraft aircraft){
+    public ResponseEntity<AircraftResponse> addAircraft(@RequestBody AircraftRequest request){
+        Aircraft aircraft= AircraftMapper.toEntity(request);
         Aircraft savedAircraft=aircraftService.saveAircraft(aircraft);
-        return ResponseEntity.ok(savedAircraft);
+        return ok(AircraftMapper.toResponse(savedAircraft));
     }
 
     @GetMapping
-    public ResponseEntity<List<Aircraft>> getAllAircraft(){
-        return ResponseEntity.ok(aircraftService.getAllAircraft());
+    public ResponseEntity<List<AircraftResponse>> getAllAircraft(){
+        List<AircraftResponse>responseList=aircraftService.getAllAircraft()
+                .stream().map(AircraftMapper::toResponse).collect(Collectors.toList());
+        return ok(responseList);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Aircraft>getAircraftById(@PathVariable Long id){
-        Optional<Aircraft> aircraft=aircraftService.getAircraftById(id);
-        return aircraft.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<AircraftResponse>getAircraftById(@PathVariable Long id){
+        return aircraftService.getAircraftById(id)
+                .map(AircraftResponse::toResponse).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
